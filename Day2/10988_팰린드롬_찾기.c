2f4e250b7f9397cb4d3639bc92a199ff011256ae
Y00nS00Hyun <yoonsooh1114@gmail.com>
2024-02-15 19:44:24 +0900

    formatting

--- Day2/10988_팰린드롬_찾기.c
+++ Day2/10988_팰린드롬_찾기.c
@@ -7,16 +7,16 @@ int main(void)
     scanf("%s", arr);
     int n;
 
-    n=strlen(arr);
+    n = strlen(arr);
 
-    for(int i=0; i<n; i++)
+    for (int i = 0; i < n; i++)
     {
-        if (arr[i]!=arr[n-1-i])
+        if (arr[i] != arr[n - 1 - i])
         {
-            printf ("0");
+            printf("0");
             return 0;
         }
     }
-    printf ("1");
+    printf("1");
     return 0;
 }
