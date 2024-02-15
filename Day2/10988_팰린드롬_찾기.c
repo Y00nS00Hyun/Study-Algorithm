e6e2fadd6367e2f54bcb645abd2c06b6c7b5ac90
Y00nS00Hyun <yoonsooh1114@gmail.com>
2024-02-15 19:34:54 +0900

    해결 완료

---
+++ Day2/10988_팰린드롬_찾기.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    char arr[100];
+    scanf("%s", arr);
+    int n;
+
+    n=strlen(arr);
+
+    for(int i=0; i<n; i++){
+        if (arr[i]!=arr[n-1-i]){
+        printf ("0");
+        return 0;
+        }
+    }
+    printf ("1");
+
+    return 0;
+}
