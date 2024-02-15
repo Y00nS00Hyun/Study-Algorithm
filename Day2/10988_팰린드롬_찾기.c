b3502f936b5084337a55f30b1dd720f69dcfa8b1
Y00nS00Hyun <yoonsooh1114@gmail.com>
2024-02-15 19:36:14 +0900

    줄 바꿈

--- Day2/10988_팰린드롬_찾기.c
+++ Day2/10988_팰린드롬_찾기.c
@@ -9,13 +9,14 @@ int main(void)
 
     n=strlen(arr);
 
-    for(int i=0; i<n; i++){
-        if (arr[i]!=arr[n-1-i]){
-        printf ("0");
-        return 0;
+    for(int i=0; i<n; i++)
+    {
+        if (arr[i]!=arr[n-1-i])
+        {
+            printf ("0");
+            return 0;
         }
     }
     printf ("1");
-
     return 0;
 }
