d898cb8c72c14777c4a4c69afc1d41c75c853353
Y00nS00Hyun <yoonsooh1114@gmail.com>
2024-02-26 16:14:36 +0900

    구구

--- Day4/배열2.c
+++ Day4/배열2.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
 int main()
 {
+    int arr[10] = {21, 32, 54, 845, 1, -6475, 789, 8745, 50, 100};
+    for (int i = 0; i < 10; i++)
+    {
+        printf("배열의 %d번째 원소: %d\n", i + 1, arr[i]);
+    }
+    return 0;
 }
