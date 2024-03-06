fbf86cd52594cbdb24cd34b53b2ca97430886e46
Y00nS00Hyun <yoonsooh1114@gmail.com>
2024-03-07 02:16:45 +0900

    ㅇㅇ

--- Day4/평균2.c
+++ Day4/평균2.c
@@ -1,21 +1,33 @@
 #include <stdio.h>
-int main() // 전체 학생의 평균 구하기
+int main()
 {
     int total;
-    printf("학생 수: ");
+    printf("전체 학생수 : ");
     scanf("%d", &total);
+    int arr[total];
     int i, ave = 0;
 
-    int arr[total];
     for (i = 0; i < total; i++)
     {
-        printf("%d 번째 학생의 성적은: ", i + 1);
+        printf("%d 번째 학생의 성적은? ", i + 1);
         scanf("%d", &arr[i]);
     }
     for (i = 0; i < total; i++)
     {
         ave = ave + arr[i];
     }
-    printf("%d", ave / total);
+
+    ave = ave / total;
+    printf("전체 학생의 평균은 : %d \n", ave);
+
+    for (i = 0; i < total; i++)
+    {
+        printf("학생 %d : ", i + 1);
+        if (arr[i] >= ave)
+            printf("합격 \n");
+        else
+            printf("불합격 \n");
+    }
+
     return 0;
 }
