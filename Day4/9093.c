82f242a84ce360c909abd1f2c4f56741f7cc4ecb
Y00nS00Hyun <yoonsooh1114@gmail.com>
2024-03-07 20:08:04 +0900

    @@

--- Day4/9093.c
+++ Day4/9093.c
@@ -4,20 +4,40 @@
 #include <string.h>
 int main()
 {
-    int n, length, count = 0;
+    int n;
     scanf("%d", &n);
     getchar(); // scanf 후 남은 개행 문자 소비
+    char temp;
+    char str[1000];
 
     for (int i = 0; i < n; i++)
     {
-        char str[1000];
+        int top = 0;
         gets(str);
-
         int length = strlen(str);
-        for (int k = 0; k < length; k++)
+
+        for (int k = 0; k <= length; k++)
         {
-            str[length - k] = str[length];
-            printf("%s", str);
+            if (str[k] == ' ' || str[k] == '\0')
+            {
+                for (int p = k - 1; p >= top;)
+                {
+                    temp = str[p];
+                    str[p] = str[top];
+                    str[top] = temp;
+                    top++;
+                    p--;
+                }
+                top = k + 1;
+            }
         }
+
+        for (int i = 0; i < length; i++)
+        {
+            printf("%c", str[i]);
+        }
+        printf("\n");
     }
+
+    return 0;
 }
