52298aa72eef4ecde036a607e1e51775e5fcd954
Y00nS00Hyun <yoonsooh1114@gmail.com>
2024-03-11 05:33:15 +0900

    @@

---
+++ Day4/새싹.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    printf(
+        "         ,r'\"7\n"
+        "r`-_   ,'  ,/\\n"
+        " \\. \". L_r'\n"
+        "   `~\\/\n"
+        "      |\n"
+        "      |\n");
+}
