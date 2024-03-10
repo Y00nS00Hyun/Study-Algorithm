202f0a14c6625b72887474fa5fd4709d23de0754
Y00nS00Hyun <yoonsooh1114@gmail.com>
2024-03-11 04:28:41 +0900

    성공!

--- Day4/3052.c
+++ Day4/3052.c
@@ -1,4 +1,25 @@
 #include <stdio.h>
 int main()
 {
+    int i, arr[10], c[10], count = 10;
+
+    for (i = 0; i < 10; i++)
+    {
+        scanf("%d", &arr[i]);
+
+        c[i] = arr[i] % 42;
+    }
+    for (i = 0; i < 9; i++)
+    {
+        for (int j = 1; i + j < 10; j++)
+        {
+            if (c[i] == c[i + j])
+            {
+                count--;
+                break;
+            }
+        }
+    }
+
+    printf("%d", count);
 }
