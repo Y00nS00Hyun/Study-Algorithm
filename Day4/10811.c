b411d3e28937ef4a27a13ecbae22320d6521e67d
Y00nS00Hyun <yoonsooh1114@gmail.com>
2024-03-10 22:32:38 +0900

    풀 수 있을까

---
+++ Day4/10811.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int n, m, i, arr[100], sw[200];
+    scanf("%d %d", &n, &m);
+
+    for (i = 0; i < 2 * m; i += 2)
+    {
+        scanf("%d %d", &sw[i], &sw[i + 1]);
+    }
+
+    for (i = 0; i < 2 * m; i += 2)
+    {
+    }
+    return 0;
+}
