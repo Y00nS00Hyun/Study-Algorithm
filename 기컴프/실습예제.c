4e9c78792cb56fc33f9b71b2d9b9bf3438a21959
Y00nS00Hyun <yoonsooh1114@gmail.com>
2025-04-14 15:28:06 +0900

    부연 설명명

--- 기컴프/실습예제.c
+++ 기컴프/실습예제.c
@@ -3,12 +3,12 @@ int main(void)
 {
     int x = 0xffff;
     printf("%6d %08x\n", x, x);
-    printf("%6d %08x\n", x >> 1, x >> 1);
-    printf("%6d %08x\n", x >> 2, x >> 2);
-    printf("%6d %08x\n", x >> 3, x >> 3);
+    printf("%6d %08x\n", x >> 1, x >> 1); // 2를 나눈 효과
+    printf("%6d %08x\n", x >> 2, x >> 2); // 2를 2번 나눈 효과
+    printf("%6d %08x\n", x >> 3, x >> 3); // 2를 3번 나눈 효과
 
-    printf("%6d %08x\n", x << 1, x << 1);
-    printf("%6d %08x\n", x << 2, x << 2);
+    printf("%6d %08x\n", x << 1, x << 1); // 2를 곱한 효과
+    printf("%6d %08x\n", x << 2, x << 2); // 2를 2번 곱한 효과
 
     return 0;
 }
