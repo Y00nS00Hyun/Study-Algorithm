4da9be0366215fc1e434c9980148360a25c15399
Y00nS00Hyun <yoonsooh1114@gmail.com>
2025-04-14 15:25:57 +0900

    밀어내기기

--- 기컴프/실습예제.c
+++ 기컴프/실습예제.c
@@ -1,4 +1,14 @@
 #include <stdio.h>
 int main(void)
 {
+    int x = 0xffff;
+    printf("%6d %08x\n", x, x);
+    printf("%6d %08x\n", x >> 1, x >> 1);
+    printf("%6d %08x\n", x >> 2, x >> 2);
+    printf("%6d %08x\n", x >> 3, x >> 3);
+
+    printf("%6d %08x\n", x << 1, x << 1);
+    printf("%6d %08x\n", x << 2, x << 2);
+
+    return 0;
 }
