ee58ec2c74cfdeb8cad4a7eb5a933f6b907afa81
Y00nS00Hyun <yoonsooh1114@gmail.com>
2025-05-27 14:33:49 +0900

    중복 없이 로또 번호 생성

--- 기컴프/로또.c
+++ 기컴프/로또.c
@@ -7,11 +7,33 @@ int main(void)
     int lotto[6];
     srand(time(NULL));
     // TODO: 중복 없이 1~45 사이의 숫자 6개를 lotto 배열에 채우기
+    int duplicate = 0;
+    int i = 0;
 
-        printf("추첨 번호: ");
+    while (i < 6)
+    {
+        int number = rand() % 45 + 1;
+        for (int j = 0; j < i; j++)
+        {
+            if (number == lotto[j])
+            {
+                duplicate = 1;
+            }
+        }
+
+        if (duplicate == 0)
+        {
+            lotto[i] = number;
+            i++;
+        }
+
+        duplicate = 0;
+    }
+
+    printf("추첨 번호: ");
     for (int i = 0; i < 6; i++)
     {
-        printf("%d", lotto[i]);
+        printf("%d ", lotto[i]);
     }
     printf("\n");
 
