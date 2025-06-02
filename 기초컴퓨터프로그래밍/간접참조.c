679b90a1b0154396c63a9e663cb49e52c940eacf
Y00nS00Hyun <yoonsooh1114@gmail.com>
2025-06-02 16:31:02 +0900

    포인터 변수와 간접연산자 *를 이용한 간접참조

--- 기초컴퓨터프로그래밍/간접참조.c
+++ 기초컴퓨터프로그래밍/간접참조.c
@@ -2,4 +2,13 @@
 int main(void)
 {
     int i = 0;
+    char c = 'A';
+
+    int *pi = &i;
+    char *pc = &c;
+    printf("간접참조 출력: %d %c\n", *pi, *pc);
+
+    *pi = 200; // 변수 i를 *pi로 간접참조하여 그 내용을 수정
+    *pc = 'B'; // 변수 c를 *pc로 간접참조하여 그 내용을 수정
+    printf("직접참조 출력: %d %c\n", i, c);
 }
