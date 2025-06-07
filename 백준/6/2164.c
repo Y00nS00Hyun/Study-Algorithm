54ae50c3da8c2d84eb4d150742ef555803456346
Y00nS00Hyun <yoonsooh1114@gmail.com>
2025-06-07 14:59:58 +0900

    큐를 사용하지 않아 런타임 에러 발생

---
+++ 백준/6/2164.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX_QUEUE_SIZE 500000
+
+int main()
+{
+    int soohyun[MAX_QUEUE_SIZE];
+    int N, temp;
+    int *ptr = &soohyun[0];
+
+    scanf("%d", &N);
+    soohyun[0] = 0;
+
+    for (int i = 1; i <= N; i++)
+    {
+        soohyun[i] = i;
+    }
+
+    for (int i = 1; i < N; i++)
+    {
+        *(ptr + N + i) = *(ptr + 2 * i);
+    }
+
+    printf("%d", *(ptr + 2 * N - 1));
+}
