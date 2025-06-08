0e6ad28cc93ed848c97dcd73aee2d82fd46cae45
Y00nS00Hyun <yoonsooh1114@gmail.com>
2025-06-09 00:30:34 +0900

    아직 엉망진창인 코드

--- 백준/6/4659.c
+++ 백준/6/4659.c
@@ -2,9 +2,50 @@
 #include <string.h>
 #include <stdlib.h>
 
+int ddaenggu(char soohyun[][20],int i){
+    int value[26];
+    int arr[20];
+    int firstkey=0;
+    for (int j = 0; j < 26; j++) {
+        value[j]=3;
+    }
+    
+    //모음
+    value['a'-'a'] = 1;
+    value['i'-'a'] = 1;
+    value['u'-'a'] = 1;
+
+    value['e'-'a'] = 1;
+    value['o'-'a'] = 1;
+
+    for (int j = 0; j < i; j++) {
+        for (int k = 0; k < 20; k++) {
+            arr[k]=soohyun[j][k]-'a';
+            if(value[arr[k]]==1){
+                if(value[arr[k]]==value[arr[k+1]]&&value[arr[k]]==value[arr[k+2]]){
+                    if(arr[k]!=arr[k+1]){
+                        firstkey=0;
+                    }
+                }
+            }else{
+                firstkey=1;
+               
+            }
+        }
+    }
+
+    if(firstkey==1){
+    printf("안돼");
+    }else{
+    printf("돼돼");
+    }
+}
+
+
 int main()
 {
     char soohyun[10000][20];
+    
     int SH=0;
     int i=0;
 
@@ -18,10 +59,6 @@ int main()
     }
 
     }
-
-    printf("입력된 단어들:\n");
-    for (int j = 0; j < i; j++) {
-        printf("%s\n", soohyun[j]);
-    }
   
+    ddaenggu(soohyun, i);
 }
