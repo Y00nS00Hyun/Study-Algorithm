b6e124ddaad1c72694c5731473a8c6fbfc17a246
Y00nS00Hyun <yoonsooh1114@gmail.com>
2025-06-09 15:30:49 +0900

    strlen 으로 문자열 길이 구하기

--- 기초컴퓨터프로그래밍/문자열문제1.c
+++ 기초컴퓨터프로그래밍/문자열문제1.c
@@ -10,12 +10,13 @@ int main()
     printf("문자열을 입력하세요: ");
     scanf("%s", str); // 문자열 입력 (공백 이후는 저장되지 않음)
 
-    // TODO.문자열 길이 직접 계산
+    // TODO. 문자열 길이 직접 계산
 
-    while (str[length] != '\0')
-    {
-        length++;
-    }
+    // while (str[length] != '\0')
+    // {
+    //     length++;
+    // }
+    length = strlen(str);
 
     printf("문자열 길이: %d\n", length);
     return 0;
