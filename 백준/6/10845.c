5ae4fb32ae9efe783f2b301af128b3cbddad7c23
Y00nS00Hyun <yoonsooh1114@gmail.com>
2025-06-15 00:33:44 +0900

    문자열 입력받고 strcmp 로 비교

--- 백준/6/10845.c
+++ 백준/6/10845.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #define MAX_QUEUE_SIZE 100001
 
 int soohyun[MAX_QUEUE_SIZE];
@@ -25,10 +26,57 @@ void push(int value)
 void pop()
 {
     if (isEmpty() == 1)
+    {
+        printf("-1\n");
+    }
+    else
+    {
+        printf("%d\n", soohyun[0]);
+        for (int i = 0; i < top - 1; i++)
+        {
+            soohyun[i] = soohyun[i + 1];
+        }
+    }
 }
 
 int main()
 {
     int N;
     scanf("%d", &N);
+
+    for (int i = 0; i < N; i++)
+    {
+        char A[10];
+        scanf("%s", A);
+
+        if (strcmp(A, "push") == 0)
+        {
+            printf("1");
+        }
+        else if (strcmp(A, "pop") == 0)
+        {
+            printf("1");
+        }
+        else if (strcmp(A, "size") == 0)
+        {
+            printf("2");
+        }
+        else if (strcmp(A, "empty") == 0)
+        {
+            printf("3");
+        }
+        else if (strcmp(A, "front") == 0)
+        {
+            printf("4");
+        }
+        else if (strcmp(A, "back") == 0)
+        {
+            printf("5");
+        }
+        else
+        {
+            printf("잘못입력하셨습니뿅");
+        }
+        printf("\n");
+    }
 }
