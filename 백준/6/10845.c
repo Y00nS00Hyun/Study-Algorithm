716496bc705e4648505a405b397078a35bdebfff
Y00nS00Hyun <yoonsooh1114@gmail.com>
2025-06-14 23:57:05 +0900

    큐 구현 중

--- 백준/6/10845.c
+++ 백준/6/10845.c
@@ -2,6 +2,31 @@
 #include <stdlib.h>
 #define MAX_QUEUE_SIZE 100001
 
+int soohyun[MAX_QUEUE_SIZE];
+int top = -1;
+
+int isEmpty()
+{
+    if (top < 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+void push(int value)
+{
+    soohyun[++top] = value;
+}
+
+void pop()
+{
+    if (isEmpty() == 1)
+}
+
 int main()
 {
     int N;
