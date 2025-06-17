0e6621297e1cf25bdf0b64c36257a8a36a5edb80
Y00nS00Hyun <yoonsooh1114@gmail.com>
2025-06-17 23:59:36 +0900

    부녀회장 화이팅

--- 백준/6/2775.c
+++ 백준/6/2775.c
@@ -2,13 +2,32 @@
 int main()
 {
     int T, k, n;
+    int arr[15][15];
     scanf("%d", &T);
+
+    for (int i = 0; i < 15; i++)
+    {
+        arr[0][i] = i + 1;
+    }
+
+    for (int i = 0; i < 15; i++)
+    {
+        for (int j = 1; j < 15; j++)
+        {
+            for (int k = 0; k < 15; k++)
+            {
+                arr[i][j] += arr[i - 1][k];
+            }
+        }
+    }
 
     for (int i = 0; i < T; i++)
     {
         scanf("%d %d", &k, &n);
-        printf("%d", i);
+        printf("%d %d", arr[k][n]);
     }
+
+    printf("%d", arr[k][n - 1]);
 
     return 0;
 }
