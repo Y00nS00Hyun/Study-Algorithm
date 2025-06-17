7b017682fad23d6ca7f4d11c913d933e41d07e7b
Y00nS00Hyun <yoonsooh1114@gmail.com>
2025-06-17 23:33:54 +0900

    부녀회장

---
+++ 백준/6/2775.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int T, k, n;
+    scanf("%d", &T);
+
+    for (int i = 0; i < T; i++)
+    {
+        scanf("%d %d", &k, &n);
+        printf("%d", i);
+    }
+
+    return 0;
+}
