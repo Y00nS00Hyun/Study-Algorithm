55008a6cd0ff9673035849a75dbdcf53390b660e
Y00nS00Hyun <yoonsooh1114@gmail.com>
2025-06-22 04:39:13 +0900

    최선의 선택을 하는 greedy 알고리즘

--- 백준/6/11047.c
+++ 백준/6/11047.c
@@ -1,5 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
+    int N, K, max;
+    int number = 0;
+    scanf("%d %d", &N, &K);
+
+    int *ptr;
+    ptr = (int *)malloc(sizeof(int) * N);
+
+    for (int i = 0; i < N; i++)
+    {
+        scanf("%d", &ptr[i]);
+    }
+
+    while (K != 0)
+    {
+        max = ptr[0];
+        for (int i = 0; i < N; i++)
+        {
+            if (K >= ptr[i])
+            {
+                if (ptr[i] >= max)
+                {
+                    max = ptr[i];
+                }
+            }
+        }
+        number += K / max;
+        K = K % max;
+        // printf("남은 돈: %d max: %d 동전 갯수: %d\n", K, max, number);
+    }
+    printf("%d", number);
+    free(ptr);
 }
