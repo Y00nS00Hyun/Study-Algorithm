cedfa3b676b0be3bf20d3ee1917fe4427c40c229
Y00nS00Hyun <yoonsooh1114@gmail.com>
2025-06-22 04:54:25 +0900

    코드 개선

--- 백준/6/11047.c
+++ 백준/6/11047.c
@@ -18,18 +18,14 @@ int main()
     while (K != 0)
     {
         max = ptr[0];
-        for (int i = 0; i < N; i++)
+        for (int i = N - 1; i >= 0; i--)
         {
             if (K >= ptr[i])
             {
-                if (ptr[i] >= max)
-                {
-                    max = ptr[i];
-                }
+                number += K / ptr[i];
+                K = K % ptr[i];
             }
         }
-        number += K / max;
-        K = K % max;
         // printf("남은 돈: %d max: %d 동전 갯수: %d\n", K, max, number);
     }
     printf("%d", number);
