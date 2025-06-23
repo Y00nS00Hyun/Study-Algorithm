684fa574f9d7ab55f99576bef8dcae0c0863a9c7
Y00nS00Hyun <yoonsooh1114@gmail.com>
2025-06-23 12:16:26 +0900

    이런것도 가능하다니 신기

--- 기초컴퓨터프로그래밍/for2.c
+++ 기초컴퓨터프로그래밍/for2.c
@@ -7,4 +7,9 @@ int main()
     {
         printf("i=%d, j=%d\n", i, j);
     }
+
+    for (int i = 0, j = 10; (i < j) && (j % 2 == 0); i++, j--)
+    {
+        printf("i=%d, j=%d\n", i, j);
+    }
 }
