33e758cd41cb9eae0f070f04f0f8a5e866c4186c
Y00nS00Hyun <yoonsooh1114@gmail.com>
2025-06-29 23:24:44 +0900

    좌표 정렬 할 필요가 없었는데 왜 했지

--- 백준/6/1711.c
+++ 백준/6/1711.c
@@ -36,8 +36,48 @@ int main()
 
     qsort(soohyun, N, sizeof(coordinate), compare);
 
+    printf("\n좌표 정렬 결과\n");
     for (int i = 0; i < N; i++)
     {
         printf("%d %d\n", soohyun[i].x, soohyun[i].y);
     }
+
+    int a, b, X, Y, Z;
+    int count = 0;
+
+    for (int i = 0; i < N - 2; i++)
+    {
+        for (int j = 1; j < N - 1; j++)
+        {
+            if (i < j)
+            {
+                a = soohyun[i].x - soohyun[j].x;
+                b = soohyun[i].y - soohyun[j].y;
+                X = a * a + b * b;
+
+                for (int k = 2; k < N; k++)
+                {
+                    if (i < k && j < k)
+                    {
+                        a = soohyun[j].x - soohyun[k].x;
+                        b = soohyun[j].y - soohyun[k].y;
+                        Y = a * a + b * b;
+
+                        a = soohyun[i].x - soohyun[k].x;
+                        b = soohyun[i].y - soohyun[k].y;
+                        Z = a * a + b * b;
+
+                        if (X + Y == Z || X + Z == Y || Z + Y == X)
+                        {
+                            count++;
+                        }
+                        printf("i: %d j: %d k: %d\n", i + 1, j + 1, k + 1);
+                        printf("X: %d Y: %d Z: %d\n\n", X, Y, Z);
+                    }
+                }
+            }
+        }
+    }
+
+    printf("%d", count);
 }
