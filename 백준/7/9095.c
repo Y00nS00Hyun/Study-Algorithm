b2a94a4fd2095fd89ca7cdb6d96e1e881bc54d15
Y00nS00Hyun <yoonsooh1114@gmail.com>
2025-07-01 05:45:36 +0900

    DP - 앞에 3개만 더해야함

--- 백준/7/9095.c
+++ 백준/7/9095.c
@@ -4,25 +4,26 @@ void soohyun(int n)
 {
     int arr[11];
     arr[0] = 0;
-    arr[1] = 0;
+    arr[1] = 1;
+    arr[2] = 2;
+    arr[3] = 4;
 
-    for (int i = 2; i < 11; i++)
+    for (int i = 4; i < 11; i++)
     {
         arr[i] = 0;
-        for (int j = 0; j < i; j++)
+        for (int j = i - 3; j < i; j++)
         {
             arr[i] += arr[j];
             // printf("i: %d j: %d\n", i, j);
         }
-        arr[i] += i - 1;
     }
 
-    for (int j = 0; j < 11; j++)
-    {
-        printf("%d\n", arr[j]);
-    }
+    // for (int j = 0; j < 11; j++)
+    // {
+    //     printf("%d\n", arr[j]);
+    // }
 
-    // printf("%d\n", arr[n]);
+    printf("%d\n", arr[n]);
 }
 
 int main()
