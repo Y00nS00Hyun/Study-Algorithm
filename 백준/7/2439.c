62ffb7c064bed8d197e79dc3897215b93d17cc29
Y00nS00Hyun <yoonsooh1114@gmail.com>
2025-07-03 18:41:36 +0900

    shooting star

---
+++ 백준/7/2439.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int N;
+    scanf("%d", &N);
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N - i - 1; j++)
+        {
+            printf(" ");
+            // printf("%d", j);
+        }
+        // printf("\n");
+        for (int j = 0; j < i + 1; j++)
+        {
+            printf("*");
+            // printf("%d", j);
+        }
+        printf("\n");
+    }
+}
