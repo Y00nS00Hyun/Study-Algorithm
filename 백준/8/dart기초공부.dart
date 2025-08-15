62bd213357865258d5e12d7498079e17e074f2b0
Y00nS00Hyun <yoonsooh1114@gmail.com>
2025-08-15 22:49:03 +0900

    List

--- 백준/8/dart기초공부.dart
+++ 백준/8/dart기초공부.dart
@@ -1,3 +1,15 @@
 void main() {
-  print('hello dart');
+  //List -> 배열인듯
+  List<String> riize = ['수현', '원빈', '성찬'];
+  List<int> numbers = [1, 2, 3, 4, 5];
+
+  print(numbers);
+  print(riize[1]);
+  print(riize.length);
+  riize.add('은석');
+  print(riize);
+  riize.remove('수현');
+  print(riize);
+  riize.remove('수현');
+  print(riize);
 }
