6e33d7d5862dad3f6b1b41bb81943b53c55e9116
Y00nS00Hyun <yoonsooh1114@gmail.com>
2025-10-03 21:28:37 +0900

    주석

--- 백준/10/3058.py
+++ 백준/10/3058.py
@@ -1,7 +1,7 @@
-for _ in range(int(input())):
-    soohyun=list(map(int, input().split()))
-    even=[]
-    for n in soohyun:
-        if n %2==0:
-            even.append(n)
-    print(sum(even), min(even))
+for _ in range(int(input())): #input회 만큼 진행
+    soohyun=list(map(int, input().split())) #soohyun list 에 담기
+    even=[] #짝수 배열 만들기
+    for n in soohyun: #n회동안 soohyun list 에서
+        if n % 2==0: #짝수인것들만 골라서
+            even.append(n) #even 배열에 넣고
+    print(sum(even), min(even)) #출력
