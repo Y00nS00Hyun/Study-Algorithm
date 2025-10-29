c4c6f96408d51c95cba6a4e2f6a38de4237cbbb1
Y00nS00Hyun <yoonsooh1114@gmail.com>
2025-10-30 04:51:35 +0900

    나중에 마저 풀기

---
+++ 백준/10/1080.py
@@ -0,0 +1,9 @@
+N, M = map(int, input().split())
+
+A = [list(map(int, input().strip())) for _ in range(N)]
+B = [list(map(int, input().strip())) for _ in range(N)]
+
+for i in range(N*M):
+    if A[i] != B[i]:
+        A[i], A[i+1], A[i+2], A[i+M], A[i+M+1],
+        A[i + M+2], A[i+2*M], A[i+2*M+1], A[i+2*M+2] -> 0, 1 교환하고 옆으로 쭉쭉 가기
