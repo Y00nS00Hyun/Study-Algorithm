bc3928d1c76b80f88c579ca73a68682096cc4320
Y00nS00Hyun <yoonsooh1114@gmail.com>
2025-11-11 17:27:53 +0900

    리턴값은 어제나 하나이다

--- 백준/11/031114.py
+++ 백준/11/031114.py
@@ -1,8 +1,6 @@
-def add_many(*args):
-    result = 0
-    for i in args:
-        result = result+i
-    return result
+def add_and_mul(a, b):
+    return a+b, a*b
 
 
-print(add_many(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+result = add_and_mul(3, 4)
+print(result)
