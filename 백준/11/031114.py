727392670256616b351c9d26803b64228a3ab679
Y00nS00Hyun <yoonsooh1114@gmail.com>
2025-11-11 17:19:56 +0900

    매개변수 지정해거 호출하기

--- 백준/11/031114.py
+++ 백준/11/031114.py
@@ -1,9 +1,16 @@
-def say():
-    return "멀바"
+def sub(a, b):
+    return a-b
 
 
-def add(a, b):
-    print("%d, %d의 합은 %d" % (a, b, a+b))
+result1 = sub(a=7, b=3)
+print(result1)
 
 
-add(3, 5)
+result1 = sub(b=3, a=7)
+print(result1)
+
+result1 = sub(7, 3)
+print(result1)
+
+result1 = sub(3, 7)
+print(result1)
