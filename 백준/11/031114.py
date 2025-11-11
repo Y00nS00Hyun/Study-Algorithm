c8b5e61162f4d5868b7d7d786291377f115eb7bb
Y00nS00Hyun <yoonsooh1114@gmail.com>
2025-11-11 17:29:44 +0900

    결과 2개

--- 백준/11/031114.py
+++ 백준/11/031114.py
@@ -2,5 +2,8 @@ def add_and_mul(a, b):
     return a+b, a*b
 
 
-result = add_and_mul(3, 4)
-print(result)
+result1, result2 = add_and_mul(3, 4)
+print(result1, result2)
+
+result3 = add_and_mul(3, 4)
+print(result3)
