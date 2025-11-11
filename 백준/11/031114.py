b8d55d64d5ef8ad326ad8918df8d230ee2edf04c
Y00nS00Hyun <yoonsooh1114@gmail.com>
2025-11-11 17:26:15 +0900

    여러 값 더하기

--- 백준/11/031114.py
+++ 백준/11/031114.py
@@ -1,15 +1,8 @@
-def sub(a, b):
-    return a-b
+def add_many(*args):
+    result = 0
+    for i in args:
+        result = result+i
+    return result
 
 
-result1 = sub(a=7, b=3)
-print(result1)
-
-result2 = sub(b=3, a=7)
-print(result2)
-
-result3 = sub(7, 3)
-print(result3)
-
-result4 = sub(3, 7)
-print(result4)
+print(add_many(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
