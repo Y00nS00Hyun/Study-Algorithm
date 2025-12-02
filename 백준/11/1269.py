f64cfed0bfc9e23e4f22e9959995bd87a7e31c9f
Y00nS00Hyun <yoonsooh1114@gmail.com>
2025-12-02 15:39:52 +0900

    set 사용

--- 백준/11/1269.py
+++ 백준/11/1269.py
@@ -1,16 +1,6 @@
 Anum, Bnum = map(int, input().split())
 
-A = list(map(int, input().split()))
-B = list(map(int, input().split()))
+A = set(map(int, input().split()))
+B = set(map(int, input().split()))
 
-if Anum < Bnum:
-    number = Anum
-else:
-    number = Bnum
-
-count = 0
-for i in range(number):
-    if A[i] in B:
-        count += 1
-
-print(Anum+Bnum-2*count)
+print(len(A-B)+len(B-A))
