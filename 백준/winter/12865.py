759fc9622e073bf8a47e01b53d89a141fd5e3064
Y00nS00Hyun <yoonsooh1114@gmail.com>
2025-12-25 23:59:01 +0900

    knapsack

--- 백준/winter/12865.py
+++ 백준/winter/12865.py
@@ -1,11 +1,17 @@
+import sys
+input = sys.stdin.readline
+
 N, K = map(int, input().split())
 
 item = [(0, 0)]  # 1번부터 쓰려고 더미 추가
-
-for i in range(N):
+for _ in range(N):
     w, v = map(int, input().split())
     item.append((w, v))
 
+dp = [[0]*(K+1) for _ in range(N+1)]
+
 for i in range(1, N+1):
+    w, v = item[i]
     for j in range(1, K+1):
-        print("배열 입력 받기까지는 완료")
+        if j < w:
+            dp[i][j] = max(dp[i][j-1], dp[i-w][j]+v)
