2131f4231c48f9c9e428744d65a62d707ac06157
Y00nS00Hyun <yoonsooh1114@gmail.com>
2025-12-29 17:32:50 +0900

    kanpsack

--- 백준/winter/12865.py
+++ 백준/winter/12865.py
@@ -12,6 +12,9 @@ dp = [[0]*(K+1) for _ in range(N+1)]
 
 for i in range(1, N+1):
     w, v = item[i]
-    for j in range(1, K+1):
-        if j < w:
-            dp[i][j] = max(dp[i][j-1], dp[i-w][j]+v)
+    for j in range(0, K+1):
+        dp[i][j] = dp[i-1][j]  # i번째 물건을 안 넣는 경우
+        if j >= w:
+            dp[i][j] = max(dp[i][j], dp[i-1][j-w] + v)  # 넣는 경우
+
+print(dp[N][K])
