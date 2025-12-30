dd4c14611f97e6d390428d5aeb3fc0091d34a5c5
Y00nS00Hyun <yoonsooh1114@gmail.com>
2025-12-30 17:36:36 +0900

    knapsack2

--- 백준/winter/14728.py
+++ 백준/winter/14728.py
@@ -6,5 +6,13 @@ for _ in range(N):
     K, S = map(int, input().split())
     TimeScore.append((K, S))
 
-for i in range(N):
-    for j in range(T):
+dp = [[0]*(T+1) for _ in range(N+1)]
+
+for i in range(1, N+1):
+    K, S = TimeScore[i]
+    for j in range(T+1):
+        dp[i][j] = dp[i-1][j]
+        if j >= K:
+            dp[i][j] = max(dp[i][j], dp[i-1][j-K]+S)
+
+print(dp[N][T])
