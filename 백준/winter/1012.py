591df87d40d3a2c14a40d7eb2183bb0fcc19c95b
Y00nS00Hyun <yoonsooh1114@gmail.com>
2026-01-11 01:25:24 +0900

    +모양일 경우 처리 불가

--- 백준/winter/1012.py
+++ 백준/winter/1012.py
@@ -1,27 +1,29 @@
-M, N, K = map(int, input().split())
+S = int(input())
+for _ in range(S):
+    M, N, K = map(int, input().split())
 
-arr = [[0]*(M+2) for _ in range(N+2)]
+    arr = [[0]*(N+2) for _ in range(M+2)]
 
-for _ in range(K):
-    X, Y = map(int, input().split())
-    arr[X+1][Y+1] = 1
+    for _ in range(K):
+        X, Y = map(int, input().split())
+        arr[X+1][Y+1] = 1
 
-for x in range(1, M+1):
-    for y in range(1, N+1):
-        if arr[x][y] == 1:
-            if arr[x+1][y] == 1:
-                arr[x][y] = 0
-            elif arr[x][y+1] == 1:
-                arr[x][y] = 0
-            elif arr[x-1][y] == 1:
-                arr[x][y] = 0
-            elif arr[x][y-1] == 1:
-                arr[x][y] = 0
+    for x in range(1, M+1):
+        for y in range(1, N+1):
+            if arr[x][y] == 1:
+                if arr[x+1][y] == 1:
+                    arr[x][y] = 0
+                elif arr[x][y+1] == 1:
+                    arr[x][y] = 0
+                elif arr[x-1][y] == 1:
+                    arr[x][y] = 0
+                elif arr[x][y-1] == 1:
+                    arr[x][y] = 0
 
-count = 0
-for x in range(M):
-    for y in range(N):
-        if arr[x][y] == 1:
-            count += 1
+    count = 0
+    for x in range(M+2):
+        for y in range(N+2):
+            if arr[x][y] == 1:
+                count += 1
 
-print(count)
+    print(count)
