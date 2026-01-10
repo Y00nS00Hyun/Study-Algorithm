af32223e0c8dbda60f7413dec982fa323c2412e2
Y00nS00Hyun <yoonsooh1114@gmail.com>
2026-01-11 00:15:19 +0900

    연습장 생성

---
+++ 백준/winter/1012.py
@@ -0,0 +1,27 @@
+M, N, K = map(int, input().split())
+
+arr = [[0]*(M+2) for _ in range(N+2)]
+
+for _ in range(K):
+    X, Y = map(int, input().split())
+    arr[X+1][Y+1] = 1
+
+for x in range(1, M+1):
+    for y in range(1, N+1):
+        if arr[x][y] == 1:
+            if arr[x+1][y] == 1:
+                arr[x][y] = 0
+            elif arr[x][y+1] == 1:
+                arr[x][y] = 0
+            elif arr[x-1][y] == 1:
+                arr[x][y] = 0
+            elif arr[x][y-1] == 1:
+                arr[x][y] = 0
+
+count = 0
+for x in range(M):
+    for y in range(N):
+        if arr[x][y] == 1:
+            count += 1
+
+print(count)
---
+++ 연습노트.py
@@ -0,0 +1,3 @@
+arr = [[0]*10 for _ in range(2)]
+
+print(arr)
