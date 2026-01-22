fa14679e32d0036bdc112f309b641c4d0e810800
Y00nS00Hyun <yoonsooh1114@gmail.com>
2026-01-23 04:42:02 +0900

    시간초과

---
+++ 백준/winter/17626.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+DP = [5]*(N+1)
+
+i = 1
+while i*i <= N:
+    DP[i*i] = 1
+    i += 1
+
+for i in range(2, N+1):
+    if DP[i] == 1:
+        continue
+    j = 1
+    while j*j < i:
+        DP[i] = min(DP[i], DP[j*j]+DP[i-j*j])
+        j += 1
+
+print(DP[N])
