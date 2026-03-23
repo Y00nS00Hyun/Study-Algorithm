12f51f11de2513f832e215b7324b800d124bdc76
Y00nS00Hyun <yoonsooh1114@gmail.com>
2026-03-23 14:04:04 +0900

    이거 다 풀었나

--- 백준/winter/1074.py
+++ 백준/winter/1074.py
@@ -1,20 +1,18 @@
 N, r, c = map(int, input().split())
-r += 1
-c += 1
 visit = 0
 
-while (r != 1 and c != 1):
-    if (r < 2**(N-1)):
-        if (c >= 2**(N-1)):  # 3번
+while (r != 0 and c != 0):
+    if (r < 2**(N-1)-1):  # 1번
+        if (c >= 2**(N-1)-1):  # 2번
             c -= 2**(N-1)
-            visit += (2**(2*N-2))*2
+            visit += 2**(2*N-2)
     else:
         r -= 2**(N-1)
-        if (c >= 2**(N-1)):  # 4번
+        if (c >= 2**(N-1)-1):  # 4번
             c -= 2**(N-1)
             visit += (2**(2*N-2))*3
-        else:  # 2번
-            visit += 2**(2*N-2)
+        else:  # 3번
+            visit += (2**(2*N-2))*2
     N -= 1
 
 print(visit)
