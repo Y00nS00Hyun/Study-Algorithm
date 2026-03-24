f07afc545f498c7ddcf3d03c38a70ef15e157c12
Y00nS00Hyun <yoonsooh1114@gmail.com>
2026-03-24 16:02:44 +0900

    연산자 끼워넣기

--- 백준/삼코/14888.py
+++ 백준/삼코/14888.py
@@ -1,10 +1,28 @@
 N = int(input())
-A = [0]*N
-operator = [0]*4
+A = list(map(int, input().split()))
+op = list(map(int, input().split()))
 
-# 여러 줄 거쳐서 입력 받기
-for i in range(N):
-    A[i] = int(input())
+minimum = 10 ** 9
+maximum = -10**9
 
-for i in range(N-1):
-    operator[i] = int(input())
+
+def dfs(depth, total, plus, minus, multiply, divide):  # depth: 몇 번째 숫자까지 사용했는지
+    global minimum, maximum
+
+    if plus != 0:
+        dfs(depth+1, total+A[depth], plus-1, minus, multiply, divide)
+    if minus != 0:
+        dfs(depth+1, total-A[depth], plus, minus-1, multiply, divide)
+    if multiply != 0:
+        dfs(depth+1, total*A[depth], plus, minus, multiply-1, divide)
+    if divide != 0:
+        dfs(depth+1, int(total/A[depth]), plus, minus, multiply, divide-1)
+
+    if depth == N:  # 숫자를 전부 다 사용했으면 끝낸다 -> 연산 한 번 끝
+        maximum = max(total, maximum)
+        minimum = min(total, minimum)
+
+
+dfs(1, A[0], op[0], op[1], op[2], op[3])
+print(maximum)
+print(minimum)
