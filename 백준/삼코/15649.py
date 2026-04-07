3f9a562bec4db96ef2e17925c683d7ab5bdc458d
Y00nS00Hyun <yoonsooh1114@gmail.com>
2026-04-08 01:52:27 +0900

    자동 정렬되는데

--- 백준/삼코/15649.py
+++ 백준/삼코/15649.py
@@ -1,18 +1,18 @@
 N, M = list(map(int, input().split()))
 arr = [False]*N
-print(arr)
 
 
 def soohyun(depth, now):
     if depth == M:
-        for i in range(len(arr)):
+        for i in range(M):
             if arr[i] == True:
-                print(i+1)
-        return
+                print(i+1, end=" ")
+        print("")
 
-    for i in range(M):
+    for i in range(N):
         if arr[i] == False:
             arr[i] = True
+           # print(arr)
             soohyun(depth + 1, i+1)
             arr[i] = False
 
