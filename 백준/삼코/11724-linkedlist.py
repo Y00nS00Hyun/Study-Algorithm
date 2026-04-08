759f541a475a42d10dab52e6fee82b4e15a9a7de
Y00nS00Hyun <yoonsooh1114@gmail.com>
2026-04-08 16:27:45 +0900

    인접 리스트로 풀기

--- 백준/삼코/11724-linkedlist.py
+++ 백준/삼코/11724-linkedlist.py
@@ -1,3 +1,7 @@
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
 N, M = map(int, input().split())
 
 # 인접 리스트 만들기
@@ -22,5 +26,6 @@ for i in range(1, N+1):
     if point[i] == False:
         point[i] = True
         count += 1
+        dfs(i)
 
 print(count)
