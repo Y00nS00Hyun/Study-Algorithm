0cf1f6a34927b410d8bb5af5ee6d4d4a840e720c
Y00nS00Hyun <yoonsooh1114@gmail.com>
2026-04-09 19:50:39 +0900

    기하학

--- 백준/삼코/11758.py
+++ 백준/삼코/11758.py
@@ -6,13 +6,30 @@ x2, y2 = map(int, input().split())
 x3, y3 = map(int, input().split())
 
 # y = ax + b
-a = (y2-y1)/(x2-x1)
-b = y1-a*x1
-soohyun = a*x3+b
+if x1 != x2:
+    a = y2-y1
+    b = y1*(x2-x1)-a*x1
+    soohyun = a*x3+b
 
-if soohyun == y3:
-    print(0)
-elif soohyun < y3:
-    print(1)
+    if soohyun == y3*(x2-x1):
+        print(0)
+    elif soohyun < y3*(x2-x1):
+        print(1)
+    else:
+        print(-1)
+
 else:
-    print(-1)
+    if y2 > y1:
+        if x3 == x1:
+            print(0)
+        elif x3 < x1:
+            print(1)
+        else:
+            print(-1)
+    else:
+        if x3 == x1:
+            print(0)
+        elif x3 < x1:
+            print(-1)
+        else:
+            print(1)
